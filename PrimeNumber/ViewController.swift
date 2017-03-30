9dbc3642f8eecda8a00c97587d1c246a2cbda584
francis830227 <francis830227@gmail.com>
2017-03-30 18:15:50 +0800

    Initial Commit

---
+++ PrimeNumber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrimeNumber
+//
+//  Created by Tseng Kai Ping on 2017/3/30.
+//  Copyright © 2017年 Tseng Kai Ping. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
