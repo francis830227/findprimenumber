ed5cab11772c283ef44c79f62511857f6351aa75
francis830227 <francis830227@gmail.com>
2017-04-15 23:29:12 +0800

    first commit

--- PrimeNumber/ViewController.swift
+++ PrimeNumber/ViewController.swift
@@ -10,16 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    
+    @IBAction func doThePrimeTest(_ sender: UIButton) {
+        if inputTextField.text == "" {
+            resultLabel.text = "Please enter a number > 0."
+            resultLabel.isHidden = false
+        }else{
+            let inputNumber = Int(inputTextField.text!)!
+            var isPrime = 1
+            //1:prime 2:not prime 0: please reenter
+            
+            if inputNumber == 0 {
+                isPrime = 0
+            }else if inputNumber == 1 {
+                isPrime = 2
+            }else {
+                for i in stride(from: 2, to: inputNumber-1, by: 1 ) {
+                    if inputNumber % i == 0 {
+                        isPrime = 2
+                    }
+                }
+            }
+            if isPrime == 0 {
+                resultLabel.text = "Please enter a number > 0."
+            }else if isPrime == 1 {
+                resultLabel.text = "That's it!"
+            }else if isPrime == 2 {
+                resultLabel.text = "\(inputTextField.text!) is not a prime."
+            }
+            resultLabel.isHidden = false
+        }
+        inputTextField.text = ""
+    }
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        inputTextField.becomeFirstResponder()
+        //直接顯示鍵盤
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+        //隱藏狀態列
+    }
 
 }
 
